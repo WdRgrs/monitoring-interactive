80aad472746db41619d7e4ef251750352c846491
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2021-06-29 13:18:12 -0500

    test

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ git push -u origin main
 
 
 git add .   -after the initial commit, these are the only git commands to continue to push
+git commit -m 'message'
 git push
 */
 
@@ -28,9 +29,20 @@ const app = express();  //This is the function that puts express to use
 app.use(express.json()); //this is used to have our website be able to take json information into the process
 
 app.get('/', function(req, res) {
-    res.sendfile(path.join(__dirname, '/public/index.html')) //res is a built in body, sendfile being a build in method to send back a file at a specific path
+    res.sendFile(path.join(__dirname, '/public/index.html')) //res is a built in body, sendfile being a build in method to send back a file at a specific path
                             //path.join - join the location of index.html to the current directory
-})          //__dirname - always the first argument of the directory at this location (monitoring-interactive)
+});         //__dirname - always the first argument of the directory at this location (monitoring-interactive)
 
 const port = process.eventNames.PORT || 4545;
 app.listen(port, function() {console.log(`Server is working on ${port}`)});   // npm start ---- after this point we can 'npm start' to see if the server will run
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'd4feaa7f479341b9a9f7025e74664475',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
